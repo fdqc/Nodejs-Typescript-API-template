aea06fb342332f65896fd30d696423ef9a8b1ca0
QFernando <f.quintana88@yahoo.com.ar>
2020-03-20 18:47:40 -0300

    Fix unused param

--- src/resources/user/controller/userController.ts
+++ src/resources/user/controller/userController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 /**
  * Get the users list
  */
-export const users = (req: Request, res: Response) => {
+export const users = (_req: Request, res: Response) => {
     // Get users
     return res.status(200).json({});
 };
