5886cd0ed6851bec876d97463964e4dcd920eb10
QFernando <f.quintana88@yahoo.com.ar>
2020-03-20 20:03:17 -0300

    Changed error type to mongoose CastError

--- src/middleware/errorHandlerMiddlewares.ts
+++ src/middleware/errorHandlerMiddlewares.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import { CastError } from 'mongoose';
 
 /**
  * Mongoose error handler:
  * kind: ObjectId
  */
-export const mongooseErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+export const mongooseErrorHandler = (error: CastError, _req: Request, res: Response, next: NextFunction) => {
     if (error.kind === 'ObjectId') {
-        res.status(400).json({ errors: [{ msg: 'resource_not_found' }] });
+        return res.status(400).json({ errors: [{ msg: 'resource_not_found' }] });
     } else {
         next(error);
     }
@@ -15,6 +16,6 @@ export const mongooseErrorHandler = (error: any, req: Request, res: Response, ne
 /**
  * General error handler
  */
-export const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({ errors: [{ msg: 'unexpected_error' }] });
+export const errorHandler = (_error: any, _req: Request, res: Response, _next: NextFunction) => {
+    return res.status(500).json({ errors: [{ msg: 'unexpected_error' }] });
 };
