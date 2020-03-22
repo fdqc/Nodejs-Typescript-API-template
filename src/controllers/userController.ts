160583d9230a34ab392c4251c8d733b037b2f71d
QFernando <f.quintana88@yahoo.com.ar>
2020-03-22 14:01:57 -0300

    Logic for getting a list of users and showing a single one from its id

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,47 @@
 import { Request, Response } from 'express';
+import { validationResult, param } from 'express-validator';
+import { UserService } from '../services/userService';
 
 /**
  * Get the users list
  */
-export const users = (_req: Request, res: Response) => {
-    // Get users
-    return res.status(200).json({});
+export const users = async (_req: Request, res: Response) => {
+    const userServiceInstance = new UserService();
+
+    const usersList = await userServiceInstance.usersList();
+    return res.status(200).json(usersList);
 };
+
+/**
+ * Get info from a particular user
+ */
+export const show = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const userServiceInstance = new UserService();
+    const user = await userServiceInstance.getUser(req.params.id);
+
+    return user ? res.status(200).json(user) : res.status(404).json({ errors: [{ msg: 'user_not_found' }] });
+};
+
+/**
+ * Returns a validations array for the specified method
+ * @param method string
+ */
+export function validate(method: string) {
+    let validations: any[] = [];
+
+    switch (method) {
+        case 'show': {
+            validations = [
+                param('id').isMongoId().withMessage('invalid_id')
+            ];
+            break;
+        }
+    }
+
+    return validations;
+}
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as userController from '../controllers/userController';
 import { jwtAuth } from '../config/passport';
+import { asyncWrapper } from '../shared/utils/utils';
 
 const api = express.Router();
 
@@ -13,6 +14,21 @@ const api = express.Router();
  * @returns {Error}  default - Unexpected error
  * @security JWT
  */
-api.get('/users', jwtAuth(), userController.users);
+api.get('/users', jwtAuth(), asyncWrapper(userController.users));
+
+/**
+ * Obtains info about a particular user
+ * @route GET /users/{id}
+ * @group Users
+ * @summary Returns a user
+ * @param {string} id.path.required - provide a user Id
+ * @returns {object} 200
+ * @returns {Error}  default - Unexpected error
+ * @security JWT
+ *
+ * @todo: try to find other way of handling auth and validate middleware
+ * as they are optionals
+ */
+api.get('/users/:id', [jwtAuth(), userController.validate('show'), asyncWrapper(userController.show)]);
 
 export = api;
