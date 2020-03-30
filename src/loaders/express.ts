74c5b5f505c3a38f823d93a35c45463732b26749
QFernando <f.quintana88@yahoo.com.ar>
2020-03-29 21:53:14 -0300

    Added cors middleware to express app

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import cors from 'cors';
 import './passport';
 import { configRoutes } from '../routes';
 import { mongooseErrorHandler, errorHandler, authErrorHandler } from '../middleware/errorHandlerMiddlewares';
@@ -13,6 +14,13 @@ export default async (app: Express) => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(passport.initialize());
 
+    // Config CORS
+    const corsOpts = config.environment === 'production' ? config.corsOptions : {};
+    app.use(cors(corsOpts));
+
+    // Enabling CORS Pre-Flight
+    app.options('*', cors());
+
     // Config app's routes
     configRoutes(app);
 
@@ -22,32 +30,35 @@ export default async (app: Express) => {
     app.use(errorHandler);
 
     // Config express-swagger-generator
-    const expressSwagger = require('express-swagger-generator')(app);
-    const options = {
-        swaggerDefinition: {
-            info: {
-                description: 'API Docs',
-                title: 'Typescript Node API',
-                version: '1.0.0',
-            },
-            basePath: '/api/v1',
-            produces: [
-                'application/json',
-                'application/xml'
-            ],
-            schemes: ['http', 'https'],
-            securityDefinitions: {
-                JWT: {
-                    type: 'apiKey',
-                    in: 'header',
-                    name: 'Authorization',
-                    description: 'Value: bearer TOKEN',
+    if (config.environment === 'development') {
+        // Serve the Docs only in development
+        const expressSwagger = require('express-swagger-generator')(app);
+        const options = {
+            swaggerDefinition: {
+                info: {
+                    description: 'API Docs',
+                    title: 'Typescript Node API',
+                    version: '1.0.0',
+                },
+                basePath: '/api/v1',
+                produces: [
+                    'application/json',
+                    'application/xml'
+                ],
+                schemes: ['http', 'https'],
+                securityDefinitions: {
+                    JWT: {
+                        type: 'apiKey',
+                        in: 'header',
+                        name: 'Authorization',
+                        description: 'Value: bearer TOKEN',
+                    }
                 }
-            }
-        },
-        basedir: __dirname, // app absolute path
-        files: ['../app/routes/*.js'] // Path to the API handle folder
-    };
+            },
+            basedir: __dirname, // app absolute path
+            files: ['../app/routes/*.js'] // Path to the API handle folder
+        };
 
-    expressSwagger(options);
+        expressSwagger(options);
+    }
 };
