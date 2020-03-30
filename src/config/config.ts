38091c442a26d7d3b5479217a622cee766b7bb97
QFernando <f.quintana88@yahoo.com.ar>
2020-03-29 21:52:25 -0300

    Added white list array of origins and exported cors options object

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,9 @@ dotenv.config();
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// Trusted origins
+const whitelist = ['http://example1.com', 'http://example2.com'];
+
 export default {
     environment: process.env.NODE_ENV,
     saltRounds: parseInt(process.env.SALT_ROUNDS, 10),
@@ -10,5 +13,15 @@ export default {
     port: process.env.PORT,
     mongo_path: process.env.NODE_ENV === 'production' ?
         process.env.MONGODB_PATH :
-        process.env.MONGODB_LOCAL_PATH
+        process.env.MONGODB_LOCAL_PATH,
+    corsOptions: {
+        origin: (origin: string, callback: Function) => {
+            if (whitelist.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                /** @todo: add an error handler for cors */
+                callback(new Error('not_allowed_by_CORS'));
+            }
+        }
+    }
 };
