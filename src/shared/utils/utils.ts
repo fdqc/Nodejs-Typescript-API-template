293e9267b36c1fd317dfde8d71833f723889313d
QFernando <f.quintana88@yahoo.com.ar>
2020-04-04 21:02:17 -0300

    Updated type on asyncWrapper input function

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,11 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
+import { AsyncFunction } from '../../types/common';
 
 /**
  * Async function wrapper to catch errors and hand them
  * to the error handling middleware
- * @param foo Function
+ * @param foo AsyncFunction
  */
-export const asyncWrapper = (foo: Function) => {
+export const asyncWrapper = (foo: AsyncFunction) => {
     return (req: Request, res: Response, next: NextFunction) => {
         foo(req, res, next).catch(next);
     };
