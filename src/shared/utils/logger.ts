814e0f8503146a6b98404bef3c52b0795a8ddce5
QFernando <f.quintana88@yahoo.com.ar>
2020-04-06 23:01:33 -0300

    Defined a basic logger for the app

---
+++ src/shared/utils/logger.ts
@@ -0,0 +1,45 @@
+import config from '../../config/config';
+import { createLogger, format, transports} from 'winston';
+const { combine, timestamp, printf } = format;
+
+/**
+ * Custom Format
+ * @todo: add the error stack to the logs
+ */
+// tslint:disable-next-line: no-shadowed-variable
+const customFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp}: level: ${level}, message: ${message}`;
+});
+
+export const log = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        customFormat
+    ),
+    transports: [
+        // - Write all logs with level `error` and below to `error.log`
+        new transports.File({ filename: './storage/logs/error.log', level: 'error' }),
+        // - Write all logs with level `info` and below to `combined.log`
+        new transports.File({ filename: './storage/logs/combined.log' })
+    ]
+});
+
+if (config.environment !== 'production') {
+    log.add(new transports.Console({
+        level: 'silly',
+        format: combine(
+            timestamp(),
+            customFormat
+        )
+    }));
+}
+
+/**
+ * Meta tags
+ */
+export enum metaTags {
+    MONGODB = 'MONGODB',
+    AUTHENTICATION = 'AUTHENTICATION',
+    UNEXPECTED = 'UNEXPECTED'
+}
