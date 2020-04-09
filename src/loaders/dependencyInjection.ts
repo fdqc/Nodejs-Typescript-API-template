7f5e3bd4107df2e364b41b3cc8900bad7d30603c
QFernando <f.quintana88@yahoo.com.ar>
2020-04-09 00:01:46 -0300

    Load permission model at dependencyInjection

--- src/loaders/dependencyInjection.ts
+++ src/loaders/dependencyInjection.ts
@@ -1,7 +1,9 @@
 import { Container } from 'typedi';
 import { UserModel } from '../models/userSchema';
+import { PermissionModel } from '../models/PermissionSchema';
 
 export default () => {
     // Store dependency
     Container.set('userModel', UserModel);
+    Container.set('permissionModel', PermissionModel);
 };
--- src/models/PermissionSchema.ts
+++ src/models/PermissionSchema.ts
@@ -11,11 +11,27 @@ import mongoose from 'mongoose';
  */
 const permissionSchema = new mongoose.Schema({
     description: String,
-    value: String,
+    value: {
+        type: String,
+        unique: true
+    },
     sub_levels: [{
         description: String,
         value: String
     }]
 }, { timestamps: true });
 
-export const PermissionModel = mongoose.model('Permission', permissionSchema);
+type SubLevel = {
+    description: string;
+    value: string;
+};
+
+type PermissionDocument = mongoose.Document & {
+    description: string;
+    value: string;
+    sub_levels: SubLevel[];
+};
+
+export type PermissionModelT = mongoose.Model<PermissionDocument>;
+
+export const PermissionModel: PermissionModelT = mongoose.model('Permission', permissionSchema);
