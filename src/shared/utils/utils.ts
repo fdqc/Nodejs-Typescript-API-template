589af5fa5765a8755523a909751218d5cb68d7ce
QFernando <f.quintana88@yahoo.com.ar>
2020-04-09 00:02:54 -0300

    Defined helper functions to validate shiro like permission strings

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -11,3 +11,25 @@ export const asyncWrapper = (foo: AsyncFunction) => {
         foo(req, res, next).catch(next);
     };
 };
+
+/**
+ * Check if value is a shiro-like string
+ * in the form of root:sub:* or root:*
+ * @todo: improve regex
+ * @param value string
+ */
+export const isShiroRootLvl = (value: string) => {
+    const regEx = /^\w+(:\w+)?:\*$/;
+    return regEx.test(value);
+};
+
+/**
+ * Check if value is a shiro-like string
+ * in the form of root:sub:sub or root:sub
+ * @todo: improve regex
+ * @param value string
+ */
+export const isShiro = (value: string) => {
+    const regEx = /^\w+(:\w+)?:\w+$/;
+    return regEx.test(value);
+};
