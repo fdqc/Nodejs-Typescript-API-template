8acf3f5ce707fcdb78b5ac753f616fee719a121c
QFernando <f.quintana88@yahoo.com.ar>
2020-04-11 05:21:55 -0300

    Added some test cases

---
+++ test/middleware/permissions.test.ts
@@ -0,0 +1,84 @@
+import 'mocha';
+import chai from 'chai';
+chai.should();
+
+import sinon from 'sinon';
+import * as httpMocks from 'node-mocks-http';
+import { checkPermissions } from '../../src/middleware/permissionsMiddleware';
+
+describe('#Middleware', () => {
+    describe('-checkPermissions()', () => {
+        it(`Next should be called with required permissions ['root:read'] and
+            req.user permissions = [root:*, 'other:*']`, () => {
+            const middleware = checkPermissions(['root:read']);
+            const req = httpMocks.createRequest();
+            const res = httpMocks.createResponse();
+            const nextSpy = sinon.spy();
+
+            req.user = { permissions: ['root:*', 'other:*'] };
+
+            middleware(req, res, nextSpy);
+            nextSpy.calledOnce.should.be.true;
+        });
+
+        it(`Next should be called with required permissions ['root:read'] and
+            req.user permissions = [root:read, 'other:*']`, () => {
+            const middleware = checkPermissions(['root:read']);
+            const req = httpMocks.createRequest();
+            const res = httpMocks.createResponse();
+            const nextSpy = sinon.spy();
+
+            req.user = { permissions: ['root:read', 'other:*'] };
+
+            middleware(req, res, nextSpy);
+            nextSpy.calledOnce.should.be.true;
+        });
+
+        it(`Next should be called with required permissions
+            ['root:read', 'other:write'] and
+            req.user permissions = [root:read, 'other:*']`, () => {
+            const middleware = checkPermissions(['root:read', 'other:write']);
+            const req = httpMocks.createRequest();
+            const res = httpMocks.createResponse();
+            const nextSpy = sinon.spy();
+
+            req.user = { permissions: ['root:read', 'other:*'] };
+
+            middleware(req, res, nextSpy);
+            nextSpy.calledOnce.should.be.true;
+        });
+
+        it(`Next should throw with required permissions ['root:write'] and
+            req.user permissions = [root:read, 'other:*']`, () => {
+            const middleware = checkPermissions(['root:write']);
+            const req = httpMocks.createRequest();
+            const res = httpMocks.createResponse();
+            const nextSpy = sinon.spy();
+
+            req.user = { permissions: ['root:read', 'other:*'] };
+
+            try {
+                middleware(req, res, nextSpy);
+            } catch (error) {
+                error.message.should.equal('unauthorized');
+            }
+        });
+
+        it(`Next should throw with required permissions
+            ['root:write', 'other:write'] and
+            req.user permissions = [root:read, 'other:read']`, () => {
+            const middleware = checkPermissions(['root:write', 'other:write']);
+            const req = httpMocks.createRequest();
+            const res = httpMocks.createResponse();
+            const nextSpy = sinon.spy();
+
+            req.user = { permissions: ['root:read', 'other:read'] };
+
+            try {
+                middleware(req, res, nextSpy);
+            } catch (error) {
+                error.message.should.equal('unauthorized');
+            }
+        });
+    });
+});
---
+++ test/shared/utils.test.ts
@@ -0,0 +1,56 @@
+import 'mocha';
+import chai from 'chai';
+chai.should();
+
+import { isShiroRootLvl, isShiro } from '../../src/shared/utils/utils';
+
+describe('#Utils test', () => {
+    describe('-isShiroRootLvl()', () => {
+        it('Should return true with root:*', () => {
+            const isValid = isShiroRootLvl('root:*');
+            isValid.should.be.true;
+        });
+
+        it('Should return true with root:sub:*', () => {
+            const isValid = isShiroRootLvl('root:sub:*');
+            isValid.should.be.true;
+        });
+
+        it('Should return false with root', () => {
+            const isValid = isShiroRootLvl('root');
+            isValid.should.be.false;
+        });
+
+        it('Should return false with root:', () => {
+            const isValid = isShiroRootLvl('root:');
+            isValid.should.be.false;
+        });
+
+        it('Should return false with root-sub', () => {
+            const isValid = isShiroRootLvl('root-sub');
+            isValid.should.be.false;
+        });
+    });
+
+    describe('-isShiro()', () => {
+        it('Should return true with root:read', () => {
+            const isValid = isShiro('root:read');
+            isValid.should.be.true;
+        });
+
+        it('Should return true with root:sub:read', () => {
+            const isValid = isShiro('root:sub:read');
+            isValid.should.be.true;
+        });
+
+        it('Should return false with root:*', () => {
+            const isValid = isShiro('root:*');
+            isValid.should.be.false;
+        });
+
+        it('Should return false with root:sub:*', () => {
+            const isValid = isShiro('root:sub:*');
+            isValid.should.be.false;
+        });
+    });
+});
