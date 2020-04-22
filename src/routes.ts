2b95f1b6b08e4ea4d2282dcacbdab241983fc07d
Fernando <f.quintana88@yahoo.com.ar>
2020-04-22 01:27:50 -0300

    Routes.ts updated to use routePrefix config attribute

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,14 @@ import { Express } from 'express';
 import userRoutes from './app/routes/userRoutes';
 import authRoutes from './app/routes/authRoutes';
 import permissionRoutes from './app/routes/permissionRoutes';
+import config from './config/config';
 
 /**
  * Config routes for the app
  * @param app Express
  */
 export const configRoutes = (app: Express) => {
-    app.use('/api/v1', userRoutes);
-    app.use('/api/v1', authRoutes);
-    app.use('/api/v1', permissionRoutes);
+    app.use(config.routePrefix, userRoutes);
+    app.use(config.routePrefix, authRoutes);
+    app.use(config.routePrefix, permissionRoutes);
 };
